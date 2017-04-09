bc31b540df9582f8203bfb94764f7cf028a52b5d
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2017-04-09 01:24:20 +0100

    Clean Up Code

--- app/src/main/java/tk/gpereira/movielist/utils/JsonUtils.java
+++ app/src/main/java/tk/gpereira/movielist/utils/JsonUtils.java
@@ -1,7 +1,5 @@
 package tk.gpereira.movielist.utils;
 
-import android.util.Log;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,17 +8,16 @@ import java.util.ArrayList;
 
 public class JsonUtils {
 
-    private static String BASE_URL = "http://image.tmdb.org/t/p/w185/";
-    private static String RESULTS_ARRAY_STRING = "results";
-    private static String POSTER_STRING = "poster_path";
-
     public static void parse(ArrayList<String> movies, String resultsJSONString) throws JSONException {
         JSONObject mJSON = new JSONObject(resultsJSONString);
+        String RESULTS_ARRAY_STRING = "results";
         if (!mJSON.has(RESULTS_ARRAY_STRING)) {
             return;
         }
         JSONArray movieArray = mJSON.getJSONArray("results");
         for (int i = 0; i < movieArray.length(); i++) {
+            String POSTER_STRING = "poster_path";
+            String BASE_URL = "http://image.tmdb.org/t/p/w185/";
             movies.add(BASE_URL + movieArray.getJSONObject(i).getString(POSTER_STRING));
         }
     }
